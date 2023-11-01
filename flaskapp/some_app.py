6ea96b9cc472f3e0b95a2ef1c5cc61cf4055b7c5
elizaveta <elizaveta.mikhno@x5.ru>
2023-11-01 05:24:00 +0300

    new stage of lab

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,8 +1,17 @@
 print("Hello world")
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+#наша новая функция сайта
+@app.route("/data_to")
+def data_to():
+    some_pars = {'user':'Ivan','color':'red'}
+    some_str = 'Hello my dear friends!'
+    some_value = 10
+#передаем данные в шаблон и вызываем его
+    return render_template('simple.html',some_str = some_str, some_value = some_value,some_pars=some_pars)
 
 # декоратор для вывода страницы по умолчанию
 @app.route("/")
